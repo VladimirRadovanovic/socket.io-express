d9144c826dc6351366d29cbc6bc0870bdba835fb
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-03-31 16:35:23 -0400

    first

---
+++ backend/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const apiRouter = require('./api');
+
+
+const router = express.Router();
+
+router.use('/api', apiRouter);
+
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+module.exports = router;
