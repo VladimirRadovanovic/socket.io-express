aa92960ce529d1c37e77e90ecc02534de104fb0b
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-03-31 17:21:48 -0400

    logout be

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,14 @@ const { setTokenCookie } = require('../../utils/auth.js');
 
 const { User } = require('../../db/models');
 const { restoreUser, requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+
+
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 
 
